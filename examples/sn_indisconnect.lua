88fd9f171f573097ecd3e4b4ee1226eb9eccecbe
TastyFrenchBun <101948982+TastyFrenchBun@users.noreply.github.com>
2022-05-13 00:44:08 +0400

    move luas to /lua, remove source code

--- examples/sn_indisconnect.lua
+++
@@ -1,8 +0,0 @@
-include("sourcenet/incoming.lua")
-
-FilterIncomingMessage(net_Disconnect, function(netchan, read, write)
-	local reason = read:ReadString()
-
-	write:WriteUInt(net_Disconnect, NET_MESSAGE_BITS)
-	write:WriteString(string.reverse(reason))
-end)
